dbb630323229f3537055e899ab64c24d7b8c554e
Amenibnh <amounabnh@gmail.com>
2024-05-15 05:10:18 +0200

    14/05/2024

--- src/app/views/dailypass/get-all-dailypass/get-all-dailypass.service.ts
+++ src/app/views/dailypass/get-all-dailypass/get-all-dailypass.service.ts
@@ -10,8 +10,8 @@ export class GetAllDailypassService {
   constructor(private http: HttpClient) { }
   private apiURL = 'http://localhost:3001';
   
-  getAllDailypass(): Observable<any> {
-    const url = `${this.apiURL}/getAllDailyPass`;
+  currentAdmin(): Observable<any> {
+    const url = `${this.apiURL}/currentAdmin`;
     const token=localStorage.getItem('token')
     // Define headers
     const headers = new HttpHeaders({
@@ -24,8 +24,8 @@ export class GetAllDailypassService {
     return this.http.get<any>(url, { headers });
   }
 
-  deleteDailyPass(id:any): Observable<any> {
-    const url = `${this.apiURL}/deleteDailyPass/${id}`;
+  getResponsableAssociationDetails(id:any): Observable<any> {
+    const url = `${this.apiURL}/getResponsableAssociationDetails/${id}`;
     const token=localStorage.getItem('token')
     // Define headers
     const headers = new HttpHeaders({
@@ -35,7 +35,48 @@ export class GetAllDailypassService {
     });
 
     // Make HTTP request with headers
-    return this.http.delete<any>(url, { headers });
+    return this.http.get<any>(url, { headers });
+  }
+
+  deletePatientPass(associationId: string, email: string) {
+    const url = `${this.apiURL}/deletePatientPass/${associationId}`;
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+  
+    return this.http.delete<any>(url, { headers, body: { email } });
+  }
+  
+
+
+  addPatientPass(associationId: any, email: string): Observable<any> {
+    const url = `${this.apiURL}/addPatientPass/${associationId}`; 
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+    // Pass both associationId and email in the request body
+    return this.http.post<any>(url, { email }, { headers });
+  }
+  
+  
+
+  
+  getPatientDailyPass(id:any): Observable<any> {
+    const url = `${this.apiURL}/getPatientDailyPass/${id}`;
+    const token=localStorage.getItem('token')
+    // Define headers
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+      // Add more headers if needed
+    });
+    
+    // Make HTTP request with headers
+    return this.http.get<any>(url, { headers });
   }
 
 }
