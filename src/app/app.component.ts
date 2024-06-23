d390424e158ed72bd35434177d4c81df8313c945
Amenibnh <amounabnh@gmail.com>
2024-06-23 19:17:13 +0100

    05/06/2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,9 +23,41 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.router.events.subscribe((evt) => {
-      if (!(evt instanceof NavigationEnd)) {
-        return;
+      if (evt instanceof NavigationEnd) {
+        const subdomain = this.extractSubdomain();
+        console.log('Subdomain:', subdomain);
+        
+        // Get the port number from window.location
+        const port = window.location.port;
+
+        // Determine the base URL based on the port
+        let baseUrl = '';
+        if (port === '4400') {
+          baseUrl = 'http://gabes.localhost:4400';
+        } else if (port === '4200') {
+          baseUrl = 'http://medenine.localhost:4200';
+        }else{
+          baseUrl = 'http://localhost:4200/404';
+        }
+
+        // Redirect if subdomain is missing
+        if (!subdomain) {
+          window.location.href = baseUrl + window.location.pathname + window.location.search;
+        }
       }
     });
+  }
+
+  private extractSubdomain(): string {
+    const hostname = window.location.hostname;
+    const parts = hostname.split('.');
+    // Check if there is a subdomain (at least 2 parts)
+    if (parts.length >= 2) {
+      // Return the first part as the subdomain
+      return parts[0];
+    } else {
+      // No subdomain
+      return '';
+    }
   }
 }
